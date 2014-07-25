6f7093b3fe6105ccd6b3006d2c26cc786c40153b
Chris Gannon <cjgannon@gmail.com>
2014-07-25 11:46:51 +0100

    added missing icons and js

---
+++ assets/scripts.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+	 // AJAXIFY LIKE BUTTONS
+	  $('.action-like').click(function(){
+	
+	  var button = this;
+	
+	  $(this).addClass('disabled');
+	  $(this).addClass('on');
+	
+	  $.post($(this).attr('the_link'), function(data) {
+		if(data.valid){
+		  $(button).find(".likecount").html(''+data.likes_count+'');
+		}else{
+		  alert(data.message);
+		}
+	  }, 'json');
+	});
+	
+	// FORM VALIDATION
+	$("#add-form, #edit-form, #add-reply form").validate();
+	
+	// SHARE BUTTONS
+	$('#shareme').sharrre({
+	  share: {
+		googlePlus: true,
+		facebook: true,
+		twitter: true
+	  },
+	  buttons: {
+		googlePlus: {size: 'tall'},
+		facebook: {layout: 'box_count'},
+		twitter: {count: 'vertical'}
+	  },
+	  enableHover: false,
+	  enableCounter: false,
+	  enableTracking: true
+	});
+
+});
